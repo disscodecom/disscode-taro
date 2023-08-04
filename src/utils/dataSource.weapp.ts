10ffdb5661d43bbb2f4fb290b48132a5b97ca687
Charmtiger <charmtiger@gmail.com>
2023-08-04 23:17:01 +0800

    fix dataSource bug

--- src/utils/dataSource.weapp.ts
+++ src/utils/dataSource.weapp.ts
@@ -12,63 +12,74 @@ export function requestHandle(config?: Record<string, unknown>) {
         _options = __beforeRequest(options)
         if (_options.then && typeof _options.then === 'function') {
           _options = await __beforeRequest(options)
-      }
-      const { contentType, uri, query, params, method, headers = {}, timeout } = _options
-      console.log('[HTTP_REQUEST] ', uri, _options)
-      let data = ''
-      if (contentType === 'FORM') {
-        for (const k in params) {
-          if (typeof params[k] !== 'undefined') {
-            if (
-              typeof params[k] !== 'string' &&
-              typeof params[k] !== 'number'
-            ) {
-              console.warn('API PARAMS 参数值只可以为字符串或数值 字段名: ' + k)
-            }
-            data += '\r\n--XXX'
-            data += `\r\nContent-Disposition: form-data; name="${k}"`
-            data += '\r\n'
-            data += `\r\n${params[k]}`
-          }
         }
-        data += '\r\n--XXX--'
-      }
+        const {
+          contentType,
+          uri,
+          query,
+          params,
+          method,
+          headers = {},
+          timeout
+        } = _options
+        console.log('[HTTP_REQUEST] ', uri, _options)
+        let data = ''
+        if (contentType === 'FORM') {
+          for (const k in params) {
+            if (typeof params[k] !== 'undefined') {
+              if (
+                typeof params[k] !== 'string' &&
+                typeof params[k] !== 'number'
+              ) {
+                console.warn(
+                  'API PARAMS 参数值只可以为字符串或数值 字段名: ' + k
+                )
+              }
+              data += '\r\n--XXX'
+              data += `\r\nContent-Disposition: form-data; name="${k}"`
+              data += '\r\n'
+              data += `\r\n${params[k]}`
+            }
+          }
+          data += '\r\n--XXX--'
+        }
 
-      const url =
-        uri +
-        (query && Object.keys(query).length > 0 ? objectToQuery(query) : '')
-      Taro.request({
-        url,
-        data: contentType === 'FORM' ? data : params,
-        header: {
-          'content-type':
-            contentType === 'JSON'
-              ? 'application/json'
-              : contentType === 'application/x-www-form-urlencoded'
-              ? 'application/x-www-form-urlencoded'
-              : 'multipart/form-data; boundary=XXX',
-          ...headers
-        },
-        method,
-        timeout: timeout || 10000,
-        success: async res => {
-          // hook
-          let response = res
-          if (__afterRequest) {
-            response = __afterRequest(response)
-            // @ts-ignore
-            if (response.then && typeof response.then === 'function') {
-              response = await __afterRequest(res)
+        const url =
+          uri +
+          (query && Object.keys(query).length > 0 ? objectToQuery(query) : '')
+        Taro.request({
+          url,
+          data: contentType === 'FORM' ? data : params,
+          header: {
+            'content-type':
+              contentType === 'JSON'
+                ? 'application/json'
+                : contentType === 'application/x-www-form-urlencoded'
+                ? 'application/x-www-form-urlencoded'
+                : 'multipart/form-data; boundary=XXX',
+            ...headers
+          },
+          method,
+          timeout: timeout || 10000,
+          success: async res => {
+            // hook
+            let response = res
+            if (__afterRequest) {
+              response = __afterRequest(response)
+              // @ts-ignore
+              if (response.then && typeof response.then === 'function') {
+                response = await __afterRequest(res)
+              }
             }
+            console.log('[HTTP_RESPONSE] ', uri, response, res)
+            resolve(response)
+          },
+          fail: e => {
+            console.log('[api fail] ', uri, e)
+            reject(e)
           }
-          console.log('[HTTP_RESPONSE] ', uri, response, res)
-          resolve(response)
-        },
-        fail: e => {
-          console.log('[api fail] ', uri, e)
-          reject(e)
-        }
-      })
+        })
+      }
     })
   }
 }
